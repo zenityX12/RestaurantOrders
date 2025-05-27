bde468c54d9c4a37da459baa65033939c398e3ee
zenityX12 <144652625+zenityX12@users.noreply.github.com>
2025-05-28 05:43:12 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+// js/config.js
+// ไฟล์นี้ใช้สำหรับเก็บ URL ของ Google Apps Script Web App ของคุณ
+// หลังจาก Deploy Google Apps Script แล้ว ให้คัดลอก URL มาใส่ที่นี่
+
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwayczarQ8q6i9mAgJAGR62kdz3Cv951ZlsOh4s5Wdayyox9It0OjNX_W8m_BQR2EAWSg/exec"; // <--- !! สำคัญ: ใส่ URL Web App ของคุณที่นี่ !!
